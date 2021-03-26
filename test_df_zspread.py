97f8c0d644e532c5fd7ae2f3f910cf73091a9a4c
Nur Liyana Hayati Rosli <rosliyana96@icloud.com>
2021-03-26 22:09:02 +0800

    20210326-01

---
+++ test_df_zspread.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 12:05:24 2020
+
+@author: RMS671214
+"""
+
--- test_floatbond.py
+++ test_floatbond.py
@@ -27,8 +27,8 @@ mybond['business_day'] = 'No Adjustment'
 mybond['date_generation'] = rd.date_gen_method[1]
 mybond['face_value'] = 100
 mybond["current_coupon"] = 2.65
-mybond['spread'] = 0.50
-mybond["margin"] = 0.50
+mybond['spread'] = 0.0
+mybond["margin"] = 0.00
 mybond["fixing_basis"] = "Same Day"
 
 # %%
@@ -53,59 +53,7 @@ df_func = rcurve.interpolation(x_axis, y_axis, float(1/366), is_function=True)
 fbond = floatbond(mybond, rate)
 pd0 = pd.DataFrame(fbond["structure"])
 risks = fbond["risks"]
-#print(fbond["risks"])
-
-# %%
-# create the discount factors
-rate = {"value_date": str(mybond["value_date"]), "st_busday": "Modified Following",
-        "st_ratebasis": "Money Market", "st_daycount": "Actual/365",
-        "lt_busday": "No Adjustment", "lt_frequency": "Semi-Annual",
-        "lt_daycount": "Actual/365",
-        "rates": {'O/N': 2.30, '1W': 2.35, '1M': 2.45, '3M': 2.55,
-                  '6M': 2.65, '12M': 2.75, '1Y': 2.70, '2Y': 2.80, '3Y': 2.90,
-                  '5Y': 3.00, '10Y': 3.10, '30Y': 3.25}}
-
-dfs = dcurve.discount_factor_gen(rate, return_type="time")
-
-# %%
-new_structures = floatbond_value(mybond["value_date"], list(structures),
-                                 mybond["spread"], mybond["day_count"],
-                                 dfs=dfs)
-pd2 = pd.DataFrame(new_structures)
-
-new_structures3 = floatbond_value(mybond["value_date"], list(structures),
-                                 1.5, mybond["day_count"],
-                                 dfs=dfs)
-pd3 = pd.DataFrame(new_structures3)
+print(fbond["risks"])
 
 
-# %%
-value = 0
-accrued = 0
-for structure in new_structures3:
-    if structure.get("pv"):
-        value += structure["pv"]
-    if structure.get("acrued"):
-        accrued.get("accrued")
 
-# %%
-spread = 0.75
-m_rate = dict(rate)
-rates = m_rate["rates"]
-for key in m_rate["rates"]:
-    m_rate["rates"][key] += spread
-
-print(m_rate)
-
-
-m_dfs = dcurve.discount_factor_gen(m_rate, return_type="time")
-
-# %%
-new_structures2 = floatbond_value2(mybond["value_date"], list(structures),
-                                   mybond["day_count"],
-                                   ref_df=dfs, market_df=m_dfs)
-pd3 = pd.DataFrame(new_structures2)
-value2 = 0
-accrued = new_structures2[0]["accrued"]
-for structure in new_structures2:
-    value2 += structure["pv"]
